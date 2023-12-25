9cc036335b14ca4d5af2f369120ea007bb2627dc
Rishabh Kapoor <rishabhkapoor88@gmail.com>
2023-12-25 18:14:59 -0500

    Cookie Cutter Template Import

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a test to understand the working of Data Version Control',
+    author='Rishabh Kapoor',
+    license='',
+)
